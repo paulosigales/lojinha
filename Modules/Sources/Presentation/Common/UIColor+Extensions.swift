b0cfe396bf7054b041ff207fab5f97ee5a6dd7b4
Paulo Sigales <paulosigales@gmail.com>
2023-06-03 15:10:22 -0300

    UIColor+Extensions

---
+++ Modules/Sources/Presentation/Common/UIColor+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor+Extensions.swift
+//  
+//
+//  Created by Paulo Sigales on 03/06/23.
+//
+
+import UIKit
+
+extension UIColor {
+    static let customPrimary = UIColor(red: 7/255, green: 80/225, blue: 59/225, alpha: 1.0)
+    static let customSecondary = UIColor(red: 255/255, green: 53/225, blue: 86/225, alpha: 1.0)
+    static let customTertiary = UIColor(red: 181/255, green: 208/225, blue: 74/225, alpha: 1.0)
+    
+    static let customBackground = UIColor.white
+    static let customDisabled = UIColor.lightGray
+    
+    static let customText = UIColor(red: 51/255, green: 51/225, blue: 51/225, alpha: 1.0)
+}
+
