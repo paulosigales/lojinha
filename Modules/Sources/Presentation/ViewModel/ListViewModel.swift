97a5e68eb956c7ca2194be76f1e38e87bda8fba8
Paulo Sigales <paulosigales@gmail.com>
2023-06-03 20:44:09 -0300

    ListViewModel

--- Modules/Sources/Presentation/ViewModel/ListViewModel.swift
+++ Modules/Sources/Presentation/ViewModel/ListViewModel.swift
@@ -5,4 +5,37 @@
 //  Created by Paulo Sigales on 03/06/23.
 //
 
-import Foundation
+import Combine
+import Network
+import Domain
+
+class ListViewModel {
+    var service: ProductListServiceProtocol
+    var productList: [Product]
+    
+    init(service: ProductListServiceProtocol = ProductListService(), productList: [Product] = []) {
+        self.service = service
+        self.productList = productList
+    }
+    
+    func numberOfItemsInSection() -> Int {
+        productList.count
+    }
+    
+    func fetchProductList() -> AnyPublisher<[Product], ServiceError> {
+        return Future<[Product], ServiceError> { promise in
+            self.service.fetch { result in
+                switch result {
+                    case .success(let productList):
+                        self.productList = productList.products
+                        promise(.success(self.productList))
+                    case .failure(let error):
+                        promise(.failure(error))
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+}
+
+
