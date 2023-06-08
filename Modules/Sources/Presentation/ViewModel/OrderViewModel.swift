8ee9d752eab829b15808240f2cbd7ebf6d0cac5d
Paulo Sigales <paulosigales@gmail.com>
2023-06-07 22:30:39 -0300

    Adjusted OrderViewModel to calculate totals when update quantity

--- Modules/Sources/Presentation/ViewModel/OrderViewModel.swift
+++ Modules/Sources/Presentation/ViewModel/OrderViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 import Network
 import Domain
 
@@ -18,10 +19,43 @@ struct OrderViewModel {
         self.product = order.product
         self.size = order.size
         self.quantity = order.quantity
+        updatePriceTotal()
+        updatePriceTotalFormatted()
     }
-
+    
+    var priceTotal: Double = 0
+    var priceTotalFormatted: String = ""
+    
     private var price: Double {
         return self.product.actualPrice.currencyToDouble
     }
+    
+    private mutating func updatePriceTotal() {
+        priceTotal = price * Double(quantity)
+    }
+    
+    private mutating func updatePriceTotalFormatted() {
+        priceTotalFormatted = priceTotal.toFormattedCurrency()
+    }
+    
+    mutating func updateQuantity(by amount: Int) {
+        let newQuantity = self.quantity + amount
+        if newQuantity >= 1 {
+            self.quantity = newQuantity
+        }
+        
+        guard let index = CartViewModel.orderList.firstIndex(where: { $0.product == product && $0.size == size }) else {
+            return
+        }
+        
+        CartViewModel.orderList[index].quantity = quantity
+        
+        updatePriceTotal()
+        updatePriceTotalFormatted()
+        
+        CartViewModel.updateCartTotal()
+        CartViewModel.orderListSubject.send(CartViewModel.orderList)
+    }
 }
+
 
